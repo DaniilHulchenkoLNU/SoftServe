a97b8f993ccd170832ca7c93f58fafb032df6dd7
DaniilHulchenko <lvov.danik@gmail.com>
2024-09-21 11:27:20 +0300

    3ю1

--- SoftServe/3/5.cs
+++ SoftServe/3/5.cs
@@ -2,18 +2,23 @@
 
 public class EventProgram
 {
+    // Определение события Show типа EventHandler
     public event EventHandler Show;
 
+    // Конструктор, где добавляются обработчики и вызывается событие
     public EventProgram()
     {
+        // Добавление методов в список вызовов события Show
         Show += Dog;
         Show += Cat;
         Show += Mouse;
         Show += Elephant;
 
+        // Вызов события
         OnShow(EventArgs.Empty);
     }
 
+    // Метод для вызова события
     protected virtual void OnShow(EventArgs e)
     {
         if (Show != null)
@@ -22,6 +27,7 @@ public class EventProgram
         }
     }
 
+    // Статические методы, которые выводят названия животных
     public static void Dog(object sender, EventArgs e)
     {
         Console.WriteLine("Dog");
@@ -42,3 +48,12 @@ public class EventProgram
         Console.WriteLine("Elephant");
     }
 }
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Создание экземпляра EventProgram, что автоматически вызывает событие
+//        EventProgram eventProgram = new EventProgram();
+//    }
+//}
